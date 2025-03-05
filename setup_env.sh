f0cfde0445c533d1532ba3f06d7a683149542de0
giangchicken <giang123md@gmail.com>
2025-03-06 00:37:12 +0700

    Update

--- setup_env.sh
+++ setup_env.sh
@@ -3,15 +3,25 @@
 # Cập nhật hệ thống
 sudo apt update -y && sudo apt upgrade -y
 
-# Cài đặt Python 3.10.11 và các gói cần thiết
-sudo apt install -y python3.10 python3.10-venv python3.10-dev python3-pip
+# Cài đặt Git
+sudo apt install -y git
 
-# Đảm bảo pip được cập nhật
-python3.10 -m pip install --upgrade pip
+# Clone repository
+git clone https://github.com/giangchicken/Similarity_calculation.git
+
+
+# Cài đặt Python 3.10 và các gói cần thiết
+sudo apt install -y python3.10 python3.10-venv python3.10-dev python3-pip
 
 # Tạo và kích hoạt môi trường ảo
 python3.10 -m venv venv
 source venv/bin/activate
+
+# Di chuyển vào thư mục của project
+cd Similarity_calculation || { echo "Clone thất bại!"; exit 1; }
+
+# Đảm bảo pip được cập nhật
+pip install --upgrade pip
 
 # Cài đặt các thư viện cần thiết từ requirements.txt
 if [ -f "requirements.txt" ]; then
